a7d113e6ba5737f2baf5a952e39f42116daa6174
Aleksandr Borodij <alexandr.borodij@gmail.com>
2020-12-29 11:43:16 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MyTableComponent } from './my-table/my-table.component';
+import { TestAddressFormComponent } from './test-address-form/test-address-form.component';
+
+
+const routes: Routes = [
+  {path: '', component: MyTableComponent},
+  {path: 'create', component: TestAddressFormComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
