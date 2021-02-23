302c3c23a1c78cdc25a9821689724361ae0a2653
Aleksandr Borodij <alexandr.borodij@gmail.com>
2021-02-23 11:24:06 +0200

    deleted TreeComponent

--- src/app/tender-view-module/tender-view.module.ts
+++ src/app/tender-view-module/tender-view.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 
-import { MatTreeModule } from '@angular/material/tree';
-
 import { SharedModule } from '../shared/shared.module';
 import { TenderViewRoutingModule } from './tender-view-routing.module';
-import { TreeComponent } from './components/tree/tree.component';
 import { SelectedTenderComponent } from './components/selected-tender/selected-tender.component';
 import { BetCreateComponent } from './components/bet-create/bet-create.component';
 import { BestBetComponent } from './components/best-bet/best-bet.component';
@@ -13,7 +10,6 @@ import { BetTableComponent } from './components/bet-table/bet-table.component';
 
 @NgModule({
   declarations: [
-    TreeComponent,
     SelectedTenderComponent,
     BetCreateComponent,
     BestBetComponent,
@@ -21,9 +17,7 @@ import { BetTableComponent } from './components/bet-table/bet-table.component';
   ],
   imports: [
     TenderViewRoutingModule,
-    SharedModule,
-
-    MatTreeModule
+    SharedModule
   ],
   exports: []
 })
