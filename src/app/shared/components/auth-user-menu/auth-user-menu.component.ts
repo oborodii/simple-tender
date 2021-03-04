0146e52fc6dfb28b1b2662c04ebbdb0bba40376a
Aleksandr Borodij <alexandr.borodij@gmail.com>
2021-03-04 08:54:31 +0200

    companyName loading fixed

--- src/app/shared/components/auth-user-menu/auth-user-menu.component.ts
+++ src/app/shared/components/auth-user-menu/auth-user-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { TranslateService } from '@ngx-translate/core';
@@ -6,6 +6,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from '../../../services/auth.service';
 import { TenderService } from '../../../services/tender.service';
 import { AbstractTenderComponent } from '../abstract-tender/abstract-tender.component';
+import { TenderUser } from '../../../types/tender-user.type';
 
 
 @Component({
@@ -13,12 +14,9 @@ import { AbstractTenderComponent } from '../abstract-tender/abstract-tender.comp
   templateUrl: './auth-user-menu.component.html',
   styleUrls: ['./auth-user-menu.component.scss']
 })
-export class AuthUserMenuComponent extends AbstractTenderComponent {
+export class AuthUserMenuComponent extends AbstractTenderComponent implements OnInit {
 
-  get companyName(): string | null {
-    const result: string | null = localStorage.getItem(this.FIREBASE.LOCAL_STORAGE_USER_DISPLAY_NAME);
-    return result ? result : null;
-  }
+  companyName: string;
 
   constructor(protected tenderService: TenderService,
               protected translateService: TranslateService,
@@ -28,9 +26,30 @@ export class AuthUserMenuComponent extends AbstractTenderComponent {
   }
 
 
+  ngOnInit(): void {
+    this.getAuthUser();
+  }
+
+
   logout(): void {
     this.authService.logout();
     this.router.navigate([this.ROUTER_URL.LOGIN]);
+  }
+
+
+  private getAuthUser(): void {
+    const userDisplayName: string | null = localStorage.getItem(this.FIREBASE.LOCAL_STORAGE_USER_DISPLAY_NAME);
+
+    if (userDisplayName) {
+      this.companyName = userDisplayName;
+    } else {
+      this.authService.getAuthUser().subscribe((authUser: TenderUser | null) => {
+        if (authUser && authUser.displayName) {
+          this.companyName = authUser.displayName;
+          localStorage.setItem(this.FIREBASE.LOCAL_STORAGE_USER_DISPLAY_NAME, this.companyName);
+        }
+      });
+    }
   }
 
 }
